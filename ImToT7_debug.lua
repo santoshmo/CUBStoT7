f25079411d28244ce7f17d33bc31434e1b36cf15
samtenka <samtenka@umich.edu>
2016-04-14 15:19:13 -0400

    fixed premature-exit error: the problem was in mturk loop (mturk data may be missing lines) uncovered in _debug.lua and fixed in _revised.lua.

--- ImToT7_debug.lua
+++ ImToT7_debug.lua
@@ -16,12 +16,15 @@ mturk_g = io.open('parts/part_click_locs.txt')
 image_path = 't7/'
 
 for j = 1,11780 do 
+    print(j)
     bb_info = split(' ', bounding_boxes_g:read())
     bb_coords = {}
     for i = 2,5 do 
         table.insert(bb_coords, tonumber(bb_info[i])) -- convert from string to num
     end
+    --[[
     bb_coords = torch.Tensor(bb_coords) -- 1x4 tensor (x,y, width,height)
+    --]]
     image_info = split(' ', images_g:read()) -- (imageid, name of image)
     class_id = tonumber(string.sub(image_info[2], 1,3)) -- species id (200 possible)
     image_id = tonumber(image_info[1]) -- image id (11780 possible)
@@ -37,23 +40,27 @@ for j = 1,11780 do
         end
         table.insert(parts, entry)
     end
+    --[[
     parts = torch.Tensor(parts)
+    --]]
     
     --  MTurk, user generated data...proceed with caution? 
     mturk_parts = {}
     for k = 1,75 do 
-        mturk_line = mturk_g:read()
-        if mturk_line==nil then break end
-        mturk_part_loc_info = split(' ', mturk_line) -- (image id, part id, x, y, visible, time); if invisible, x=y=0.
-                                                     -- part id's range from 1 through 15.
-                                                     -- time: how many seconds for MTurkers to label part.
+        ss = mturk_g:read()
+        if ss==nil then break end
+        mturk_part_loc_info = split(' ', ss) -- (image id, part id, x, y, visible, time); if invisible, x=y=0.
+                                                         -- part id's range from 1 through 15.
+                                                         -- time: how many seconds for MTurkers to label part.
         entry = {}
         for l = 3,6 do
             table.insert(entry, mturk_part_loc_info[l])
         end
         table.insert(mturk_parts, entry)
     end
+    --[[
     mturk_parts = torch.Tensor(mturk_parts)
+    --]]
     
     -- write data to object and save 
     info = {}
@@ -64,5 +71,7 @@ for j = 1,11780 do
     info.parts = parts
     info.mturk_parts = mturk_parts
     filename = 'image' .. image_id .. '.t7'
+    --[[
     torch.save(image_path .. filename, info)
+    --]]
 end
